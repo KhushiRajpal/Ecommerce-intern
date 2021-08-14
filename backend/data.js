daea7fb9dc48c9512e12fb3061f81accf3f4fe8d
KhushiRajpal <khushik.rajpal2015@gmail.com>
2021-08-15 01:42:24 +0530

    added product route and nodemon package

---
+++ backend/data.js
@@ -0,0 +1,46 @@
+export default {
+	products: [
+	  {
+		_id: '1',
+		name: 'Pretty Shirt 1',
+		category: 'Shirts',
+		image: '/images/d1.jpg',
+		price: 500,
+		brand: ' Nike',
+		rating: 4.0,
+		numReviews: 10,
+		countInStock: 6,
+	  },
+	  {
+		_id: '2',
+		name: 'Pretty Shirt 2',
+		category: 'Shirts',
+		image: '/images/d1.jpg',
+		price: 750,
+		brand: ' Nike',
+		rating: 5.0,
+		numReviews: 5
+	  },
+	  {
+		_id: '3',
+		name: 'Best Pants',
+		category: 'Pants',
+		image: '/images/d1.jpg',
+		price: 70,
+		brand: ' Nike',
+		rating: 4.5,
+		numReviews: 8,
+		countInStock: 6,
+	  }, {
+		_id: '4',
+		name: 'Best Pants',
+		category: 'Pants',
+		image: '/images/d1.jpg',
+		price: 70,
+		brand: ' Nike',
+		rating: 4.5,
+		numReviews: 8,
+		countInStock: 6,
+	  },
+	]
+  }
