4acaa87f703b77c400260c182adea7d0a70a529a
KhushiRajpal <khushik.rajpal2015@gmail.com>
2021-08-15 22:22:45 +0530

    excluding login register products from jwt

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const app = express();
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const authJwt = require('./helpers/jwt');
-
+const errorHandler = require('./helpers/error-handler');
 const productsRouter = require('./routers/products.js');
 const catalogRouter = require('./routers/catalogs.js');
 const userRouter = require('./routers/users.js');
@@ -16,6 +16,7 @@ const api = process.env.API_URL;
 app.use(express.json());
 app.use(morgan('tiny'));
 app.use(authJwt());
+app.use(errorHandler)
 
 //routers
 
