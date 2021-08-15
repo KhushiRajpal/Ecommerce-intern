7b18c8c6efc73893504321fdcac0f0cee985d631
KhushiRajpal <khushik.rajpal2015@gmail.com>
2021-08-15 15:33:14 +0530

    delete and getcatalogbyid

--- backend/routers/catalogs.js
+++ backend/routers/catalogs.js
@@ -3,14 +3,27 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req, res) =>{
-	const catalogList = await catalogList.find();
+	const catalogList = await Catalog.find();
 
 	if(!catalogList) {
 		res.status(500).json({success: false})
 	}
-	res.send(catalogList);
+	res.status(200).send(catalogList);
 })
 
+//adding a get by id for a catalog
+router.get('/:id' , async(req,res) =>{
+	const catalog = await Catalog.findById(req.params.id);
+	if(!catalog)
+	{
+	return res.status(404).send('the catalog with given id was not found')
+	}
+	res.status(200).send(catalog);
+
+
+})
+
+//doing it via async 
 router.post('/', async(req,res) =>{
 	let catalog = new Catalog(
 	{
@@ -25,5 +38,20 @@ router.post('/', async(req,res) =>{
 	}
 	res.send(catalog);
 })
+
+//doing it via promise
+router.delete('/:id', (req, res) =>{
+	Catalog.findByIdAndRemove(req.params.id).then(catalog =>{
+		if(catalog) {
+			return res.status(200).json({success: true, message: 'the catalog was removed'})
+		}
+			else {
+				return res.status(404).json({success: false, message: 'the catalog was not removed'})
+			}
+		}).catch(err=>{
+			return res.status(400).json({success: false, error:err})
+		})
+
+	})
 
 module.exports =router;
