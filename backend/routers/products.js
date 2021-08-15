1ec12d712adce94086e251874c0fb5f4bac45268
KhushiRajpal <khushik.rajpal2015@gmail.com>
2021-08-15 18:49:17 +0530

    delete update verify products api

--- backend/routers/products.js
+++ backend/routers/products.js
@@ -2,9 +2,10 @@ const { Product} = require('../models/product');
 const { Catalog } = require('../models/catalog');
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 
 router.get(`/` , async (req, res) => {
-	const productList = await Product.find().select('name price catalog -_id').populate('catalog');   
+	const productList = await Product.find().populate('catalog');   
 	if(!productList){
 		res.status(500).json({success: false});
 	}
@@ -33,6 +34,48 @@ router.post(`/`,  async (req, res) =>{
     return res.status(500).send('The product cannot be created')
 	}
     res.send(product);
+})
+
+
+router.put('/:id',async (req, res)=> {
+	if(! mongoose.isValidObjectId(req.params.id)) {
+		return res.status(400).send('Invalid Product Id')
+	 }
+    const catalog = await Catalog.findById(req.body.catalog);
+    if(!catalog)
+	{
+		return res.status(400).send('Invalid Catalog! Enter the correct one')
+	} 
+    const product = await Product.findByIdAndUpdate(
+		req.params.id,
+		{ 
+			name: req.body.name,
+			description: req.body.description,
+			brand: req.body.brand,
+			price: req.body.price,
+			catalog: req.body.catalog,
+        },
+        { new: true}
+    )
+
+    if(!product)
+	{
+    return res.status(500).send('the product cannot be updated!')
+	}
+
+    res.send(product);
+})
+
+router.delete('/:id', (req, res)=>{
+    Product.findByIdAndRemove(req.params.id).then(product =>{
+        if(product) {
+            return res.status(200).json({success: true, message: 'the product is deleted!'})
+        } else {
+            return res.status(404).json({success: false , message: "product not found!"})
+        }
+    }).catch(err=>{
+       return res.status(500).json({success: false, error: err}) 
+    })
 })
 
 module.exports = router;
