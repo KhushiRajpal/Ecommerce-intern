2b8b4e62c96c414f8cd454f6f34686608f20b607
KhushiRajpal <khushik.rajpal2015@gmail.com>
2021-08-15 11:17:12 +0530

    adding db connection mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
-
 const app = express();
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 require('dotenv/config');
 const api = process.env.API_URL;
-//Middleware
+
 app.use(express.json());
+app.use(morgan('tiny'));
 
 
 app.get(`${api}/products` , (req, res) => {
@@ -23,6 +25,17 @@ app.post(`${api}/products` , (req, res) => {
 	res.send(newProduct);
 })
 
+mongoose.connect(process.env.CONNECTIONSTRING,{
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	dbName: 'eShop-database'
+})
+.then(() =>{
+	console.log('Database connection is ready...')
+})
+.catch(()=>{
+	console.log(err);
+})
 app.listen(3000, () => {
 	console.log(api);
 	console.log(`Serve at http://localhost:3000`);
